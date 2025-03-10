1b129c79f3274de609b4b7adcc7ddfdb50c3c235
getimad <107067409+getimad@users.noreply.github.com>
2025-03-10 15:55:32 +0000

    chore: Add header to the form

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,7 +1,12 @@
 import FlexibleForm from './FlexibleForm'
 
 const Home = () => {
-    return <FlexibleForm />
+    return (
+        <>
+            <h1 className="mb-6">Flexible Form</h1>
+            <FlexibleForm />
+        </>
+    )
 }
 
 export default Home
